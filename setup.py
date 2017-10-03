b04e288f602f515f4afe3b98d6881ab168b621b9
Ashley Waite <ashley.c.waite@gmail.com>
2017-10-03 15:14:13 +1100

    Updated setup

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup
 
 setup(
-    name="django-more",
-    version="0.1",
-    description="Django with more",
-    long_description=open("README.md").read(),
+    name='django-more',
+    version='0.1',
+    author='Ashley Waite',
+    author_email='ashley.c.waite@gmail.com',
+    description='Django with more',
+    long_description=open('README.md').read(),
     packages=[
-        "django-cte"
+        'patchy',
+        'django-more',
+        'django-more.storages',
+        'django-cte',
+        'django-enum'
     ],
     install_requires=[
-        "django"
+        'django'
     ]
 )
