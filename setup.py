e096f9ba508d3ad354a2164061b432f5532e6e8c
Ashley Waite <ashley.c.waite@gmail.com>
2017-10-26 12:23:33 +1100

    New details added to setup.py for PyPI

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     author_email='ashley.c.waite@gmail.com',
     description='Django with more',
     long_description=open('README.md').read(),
+    url='https://github.com/ashleywaite/django-more',
     packages=[
         'patchy',
         'django_more',
@@ -25,7 +26,18 @@ setup(
     install_requires=[
         'django',
     ],
+    python_requires='>=3.4',
     test_suite = 'tests.runtests.runtests',
     tests_require=test_deps,
     extras_require=extras,
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
 )
